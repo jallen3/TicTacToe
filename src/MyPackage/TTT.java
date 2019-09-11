a09f668d5d7a87c6c50d6f8a1bc7183f7f69e175
Jada Allen <jadaallen99@gmail.com>
2019-09-11 15:44:20 -0400

    Step 1-2

--- src/MyPackage/TTT.java
+++ src/MyPackage/TTT.java
@@ -1,4 +1,26 @@
 package MyPackage;
 
 public class TTT {
+
+    private char[][] board; //3 x 3 board layout
+    private char currentPlayerMark; //holds 'x' or 'o'
+
+    public TTT(){}
+
+    public void initializeBoard(){}
+
+    public boolean isBoardFull{}
+
+    public boolean checkForWin(){}
+
+    private boolean checkRowsforWin(){}
+
+    private boolean checkColumnsForWin(){}
+
+    private boolean checkDiagnolsForWin(){}
+
+    private boolean checkRowCol(char c1, char c2, char c3){}
+
+
+
 }
