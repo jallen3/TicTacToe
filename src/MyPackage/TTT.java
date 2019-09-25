62e4a401281e071b93cdcf655627ddba9bfd3b88
Jada Allen <jadaallen99@gmail.com>
2019-09-25 14:38:07 -0400

    Checking for a Winner (Part 1)

--- src/MyPackage/TTT.java
+++ src/MyPackage/TTT.java
@@ -1,6 +1,7 @@
 package MyPackage;
 
-public class TTT {
+public class TTT
+{
 
     private char[][] board; //3 x 3 board layout
     private char currentPlayerMark; //holds 'x' or 'o'
@@ -8,23 +9,29 @@ public class TTT {
     public TTT(){}
 
     //set/reset the board back to all empty values
-    public void initializeBoard(){
+    public void initializeBoard()
+    {
         //Loop through rows
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < 3; i++)
+        {
             //Loop through columns
-            for(int j = 0; j < 3; j++){
+            for(int j = 0; j < 3; j++)
+            {
                 board[i][j] = '-';
             }
         }
     }
 
-    public void printBoard(){
+    public void printBoard()
+    {
 
         System.out.println("-------------");
 
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < 3; i++)
+        {
             System.out.println("| ");
-            for(int j = 0; j < 3; j++){
+            for(int j = 0; j < 3; j++)
+            {
                 System.out.print(board[i][j] + " | ");
             }
 
@@ -35,13 +42,37 @@ public class TTT {
 
     public boolean isBoardFull(){}
 
-    public boolean checkForWin(){}
+    public boolean checkForWin()
+    {
+        return checkRowsForWin() || checkColumnsForWin() || checkDiagonalsForWin() ;
+    }
 
-    private boolean checkRowsforWin(){}
+    private boolean checkRowsForWin()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(checkRowsForWin((board[i][0], board[i][2], board[i][2]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    private boolean checkColumnsForWin(){}
+    private boolean checkColumnsForWin()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(checkColumnsForWin((board[0][i], board[1][i], board[2][i]) == true) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    private boolean checkDiagnolsForWin(){}
+    private boolean checkDiagonalsForWin()
+    {
+        return(checkRowCol(board[0][0], board[1][1], board[2][2]) == true || checkRowCol(board[0][2], board[1][1], board[2][0]) == true);
+    }
 
     private boolean checkRowCol(char c1, char c2, char c3){}
 
