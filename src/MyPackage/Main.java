8f7388c5e13a5568ce04e6d8d0c34c2f5752d8c1
Jada Allen <jadaallen99@gmail.com>
2019-09-30 22:22:01 -0400

    Driver class

--- src/MyPackage/Main.java
+++ src/MyPackage/Main.java
@@ -1,4 +1,37 @@
 package MyPackage;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void majn(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        TTT game = new TTT();
+        game.initializeBoard();
+        System.out.println("Tic-Tac-Toe!");
+        do {
+            System.out.println("Current board layout:");
+            game.printBoard();
+            int row;
+            int col;
+            do {
+                System.out.println(("Player" + game.getCurrentPlayerMark() + ", enter an empty row and column to place your mark!"));
+                row = myScanner.nextInt() - 1;
+                col = myScanner.nextInt() - 1;
+            }
+            while (!game.placeMark(row, col));
+            game.changePlayer();
+        }
+        while (!game.checkForWin() && !game.isBoardFull());
+        if (game.isBoardFull() && !game.checkForWin()) {
+            System.out.println("The game was a tie!");
+        } else {
+            System.out.println("Current board layout:");
+            game.printBoard();
+            game.changePlayer();
+            System.out.println(Character.toUpperCase(game.getCurrentPlayerMark()) + " Wins!");
+        }
+    }
+
 }
